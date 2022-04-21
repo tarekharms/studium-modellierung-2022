10b49644b6f082dd6b0ed46ad2ede8f28e826b08
Tarek Harms <tarek.harms@doepke.de>
2022-04-21 14:58:26 +0200

    Update Benutzer

--- src/aufgabenblatt_6/Benutzer.java
+++ src/aufgabenblatt_6/Benutzer.java
@@ -5,5 +5,18 @@ public class Benutzer {
     public static void main(String[] args)
     {
         System.out.println("Hello wordl");
+
+        Girokonto girokonto = new Girokonto();
+        Sparkonto sparkonto = new Sparkonto();
+
+        girokonto.einzahlen(1000);
+        sparkonto.einzahlen(500);
+
+        girokonto.ueberweisen(sparkonto, 200);
+        girokonto.auszahlen(150);
+        sparkonto.auszahlen(50);
+
+        System.out.println("Girokonto: " + girokonto.getKontostand());
+        System.out.println("Sparkonto: " + sparkonto.getKontostand());
     }
 }
